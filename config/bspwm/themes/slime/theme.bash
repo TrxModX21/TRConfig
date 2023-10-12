2735d9da0ed14b963198b6b8169897851640e4ac
TrxModX21 <trxcode21@gmail.com>
2023-10-12 12:07:47 +0700

    add bspwm themes config

---
+++ config/bspwm/themes/slime/theme.bash
@@ -0,0 +1,82 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2023 Aditya Shakya <adi1090x@gmail.com>
+#
+# Slime Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#f8f8f2'
+foreground='#282a36'
+color0='#282a36'
+color1='#ff5555'
+color2='#50fa7b'
+color3='#ffb86c'
+color4='#6272a4'
+color5='#bd93f9'
+color6='#8be9fd'
+color7='#44475a'
+color8='#282a36'
+color9='#ff5555'
+color10='#50fa7b'
+color11='#ffb86c'
+color12='#6272a4'
+color13='#bd93f9'
+color14='#8be9fd'
+color15='#44475a'
+
+accent='#50fa7b'
+light_value='0.40'
+dark_value='0.10'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='Iosevka Nerd Font:size=10;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Papirus-Apps'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='slime.conf'
+geany_font='JetBrains Mono 10'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Slime'
+icon_theme='Luv-Folders'
+cursor_theme='Qogirr-Dark'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x40'
+dunst_origin='top-right'
+dunst_font='Iosevka Custom 9'
+dunst_border='0'
+dunst_separator='2'
+
+# Picom
+picom_backend='glx'
+picom_corner='0'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='none'
+picom_blur_strength='0'
+
+# Bspwm
+bspwm_fbc="$accent"
+bspwm_nbc="$background"
+bspwm_abc="$color5"
+bspwm_pfc="$color2"
+bspwm_border='2'
+bspwm_gap='10'
+bspwm_sratio='0.50'
---
+++ config/bspwm/themes/trxmod21x/theme.bash
@@ -0,0 +1,82 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2023 Aditya Shakya <adi1090x@gmail.com>
+#
+# Forest Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#282a36'
+foreground='#f8f8f2'
+color0='#44475a'
+color1='#ff5555'
+color2='#50fa7b'
+color3='#f1fa8c'
+color4='#42a5f5'
+color5='#bd93f9'
+color6='#8be9fd'
+color7='#bfbaac'
+color8='#4a697d'
+color9='#fb8784'
+color10='#70d675'
+color11='#ffe744'
+color12='#51b4ff'
+color13='#c979d7'
+color14='#5cdff0'
+color15='#fdf6e3'
+
+accent='#50fa7b'
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='Iosevka Nerd Font:size=8;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Papirus-Apps'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='8'
+
+# Geany
+geany_colors='adapta.conf'
+geany_font='JetBrains Mono 10'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Adapta-Nokto'
+icon_theme='Archcraft-Dark'
+cursor_theme='Pear'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x40'
+dunst_origin='top-right'
+dunst_font='Iosevka Custom 9'
+dunst_border='2'
+dunst_separator='2'
+
+# Picom
+picom_backend='glx'
+picom_corner='21'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='kawase'
+picom_blur_strength='6'
+
+# Bspwm
+bspwm_fbc="$color0"
+bspwm_nbc="$background"
+bspwm_abc="$color5"
+bspwm_pfc="$color2"
+bspwm_border='1'
+bspwm_gap='12'
+bspwm_sratio='0.50'
